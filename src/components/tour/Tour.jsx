0e4d007ad7ee6b00283a520ccdc14f7fb9160ca7
cleverboss01 <ndubeansdaniel97@gmail.com>
2022-09-09 09:48:13 +0100

    vine

--- src/components/tour/Tour.jsx
+++ src/components/tour/Tour.jsx
@@ -4,6 +4,9 @@ const Tour = () => {
   return (
     <div className="tour" id="tour">
       <h3>Tour</h3>
+      <div className="content">
+        <p>Tour dates go here...</p>
+      </div>
     </div>
   );
 };
