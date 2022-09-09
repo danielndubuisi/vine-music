e5ab0fa8e9583188b862a8b2a3236b4ac4f3ddda
cleverboss01 <ndubeansdaniel97@gmail.com>
2022-09-09 20:49:51 +0100

    vine

--- src/components/music/Music.jsx
+++ src/components/music/Music.jsx
@@ -1,5 +1,6 @@
+import { useState } from "react";
 import { FaSpotify } from "react-icons/fa";
-import { FcLike } from "react-icons/fc";
+import { FcLike, FcLikePlaceholder } from "react-icons/fc";
 import { MdOutlineLiveTv } from "react-icons/md";
 import Button from "../../components/button/Button";
 import Img from "../../asset/empty-shell.jpeg";
@@ -7,6 +8,9 @@ import jay from "../../asset/jay.jpg";
 import "./Music.scss";
 
 const Music = () => {
+  const [click, setClick] = useState(false);
+  const [love, setLove] = useState(false);
+
   return (
     <div className="music" id="music" data-aos="fade-left">
       <h3 className="header">Music</h3>
@@ -16,18 +20,26 @@ const Music = () => {
           <div className="deluxe-poster">
             <img src={Img} alt="shell" className="poster" />
             <FaSpotify className="icon" />
-            <FcLike className="like" />
           </div>
-          <Button />
+          <div className="listen">
+            <Button />
+            <div className="like" onClick={() => setClick(!click)}>
+              {click ? <FcLike /> : <FcLikePlaceholder />}
+            </div>
+          </div>
         </div>
         <div className="live">
           <p>Hold you dear (Live version)</p>
           <div className="live-poster">
             <img src={jay} alt="shell" className="poster" />
             <MdOutlineLiveTv className="icon" />
-            <FcLike className="like" />
           </div>
-          <Button />
+          <div className="listen">
+            <Button />
+            <div className="like" onClick={() => setLove(!love)}>
+              {love ? <FcLike /> : <FcLikePlaceholder />}
+            </div>
+          </div>
         </div>
       </div>
       <div className="new">
