d135c7b9a928920dad5362b70b7b0eaaf4ffc217
cleverboss01 <ndubeansdaniel97@gmail.com>
2022-09-09 12:52:31 +0100

    vine

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,67 @@
+import {
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+  FaYoutube,
+  FaSpotify,
+} from "react-icons/fa";
+import { FiMusic } from "react-icons/fi";
+import { SiAudiomack } from "react-icons/si";
 import "./Contact.scss";
 
 const Contact = () => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("submitted");
+  };
   return (
     <div className="contact" id="contact">
-      <h3>Contact part</h3>
+      <h3 className="header">Contact</h3>
+      <div className="content">
+        <div className="top-content">
+          <div className="management">
+            <h4 className="title">Management:</h4>
+            <p className="email">daniel@vinemusic.com</p>
+            <p className="email">john@vinemusic.com</p>
+          </div>
+          <div className="label">
+            <h4 className="title">Label:</h4>
+            <p className="label-name">
+              &copy; <span>vinemusic</span>
+            </p>
+          </div>
+          <div className="social">
+            <h4 className="title">Social:</h4>
+            <div className="links">
+              <FaInstagram className="icon" />
+              <FaFacebook className="icon" />
+              <FaTwitter className="icon" />
+              <FaYoutube className="icon" />
+              <FaSpotify className="icon" />
+              <FiMusic className="icon" />
+              <SiAudiomack className="icon" />
+            </div>
+          </div>
+        </div>
+        <div className="news-letter">
+          <div className="letter-header">
+            <h3>Get notified</h3>
+            <p>Sign up with your email address to recieve news and updates</p>
+          </div>
+          <div className="form">
+            <form onClick={onSubmit}>
+              <input type="text" placeholder="First name" />
+              <input type="text" placeholder="First name" />
+              <input type="text" placeholder="First name" />
+              <button type="submit" className="signup-btn">
+                SIGN UP
+              </button>
+            </form>
+            <small>We respect your privacy.</small>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,14 @@
+import {} from "react-icons/fa";
 import "./Footer.scss";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <div className="content">
+        <p className="text">&copy; Vinemusic 2022 </p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
