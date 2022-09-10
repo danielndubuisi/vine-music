b33eb4852ab78493aad2f2ea62c8fee00d8f0efe
cleverboss01 <ndubeansdaniel97@gmail.com>
2022-09-10 04:29:37 +0100

    vine

--- src/pages/Bio.js
+++ src/pages/Bio.js
@@ -16,16 +16,16 @@ const Bio = () => {
             have unreleased full-length albums and headlined live performances,
             picking up accolades from the likes of Sanmi, and many more outlets
             along the way. <br />
-            <br /> They’ve been described, at various times over the past
-            decade, as a folk, alternative, and rock band – and the truth is
-            they’ve dabbled in all these areas, and more – but fitting into one
-            box has never been a defining factor for them. <br /> <br />
+            <br /> They’ve been described, at various times over this period, as
+            a pop, alternative, and rock band – and the truth is they’ve dabbled
+            in all these areas, and more – but fitting into one box has never
+            been a defining factor for them. <br /> <br />
             “For me, our sound is more like a concept or an existential
-            experience than it is a specific genre,” John says. <br /> <br />
+            experience than it is a specific genre,” - John says. <br /> <br />
             “If you have to boil it down that’s fine, but I want it to be more
             than that: I want it to be a way of understanding oneself, about
             making a positive impact – of making a difference in people’s lives
-            – and nothing does that more clearly than with music" Daniel says.{" "}
+            – and nothing does that more clearly than with music" - Daniel says.{" "}
             <br /> <br />
             V.I.N.E. extends beyond just the duo. There’s something there for
             you in the songs. There’s something you can find for yourself, no
